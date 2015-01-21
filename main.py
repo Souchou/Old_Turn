3799f7de46a40a0a6fc1c95627ae72b903bf7ae1
Low Kururu <stupidfool9413@gmail.com>
2015-01-21 12:12:23 +0800

    don't deal me

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 from HTMLParser import HTMLParser
 from bs4 import BeautifulSoup
 import urllib2
-from sites import TianYa
+import json
+from sites import Site
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -18,13 +19,99 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
+def newsite():
+    print "enter rules to get text of your book, empty for 'None' for each information."
+    name = raw_input('name: ')
+    if name == "":
+        name = None
+    root_ele = raw_input('parent element of the chapter links: ')
+    if root_ele == "":
+        root_ele = None
+    root_attr = raw_input('attr of the element: ')
+    if root_attr == "":
+        root_attr = None
+    root_value = raw_input('value of the attr: ')
+    if root_value == "":
+        root_value = None
+    link_ele = raw_input('element of the chapter links: ')
+    if link_ele == "":
+        link_ele = None
+    link_attr = raw_input('attr of the element: ')
+    if link_attr == "":
+        link_attr = None
+    link_value = raw_input('value of the attr: ')
+    if link_value == "":
+        link_value = None
+    ele = raw_input('parent element of the chapter: ')
+    if ele == "":
+        ele = None
+    attr = raw_input('attr of the element: ')
+    if attr == "":
+        attr = None
+    value = raw_input('value of the attr: ')
+    if value == "":
+        value = None
+    son_ele = raw_input('element(s) of the chapter: ')
+    if son_ele == "":
+        son_ele = None
+    son_attr = raw_input('attr of the element: ')
+    if son_attr == "":
+        son_attr = None
+    son_value = raw_input('value of the attr: ')
+    if son_value == "":
+        son_value = None
+    jsonfile = open('sites.json')
+    jsondata = json.load(jsonfile)
+    jsonfile.close()
+    sites = jsondata["sites"]
+    count = len(sites)
+    num = count + 1
+    dic = {
+                  "id":num,
+                  "name": name,
+                  "root_ele": root_ele,
+                  "root_attr": root_attr,
+                  "root_value": root_value,
+                  "link_ele": link_ele,
+                  "link_attr": link_attr,
+                  "link_value": link_value,
+                  "ele": ele,
+                  "attr": attr,
+                  "value": value,
+                  "son_ele": son_ele,
+                  "son_attr": son_attr,
+                  "son_value": son_value
+                  }
+    sites.append(dic)
+    newjson = json.dumps(jsondata)
+    jsfile = open("sites.json", 'w')
+    jsfile.write(newjson)
+    jsfile.close()
+    get_text()
+
 def get_site():
-    select = raw_input('Which website is the book from?\n1, tianya; 2, douban: ')
-    if select == '1':
-        site = TianYa()
-        return site
-    else:
-        print 'this site is not supported yet'
+    jsonfile = open('sites.json')
+    jsondata = json.load(jsonfile)
+    jsonfile.close()
+    all_sites = ""
+    sites = jsondata["sites"]
+    for site in sites:
+        all_sites += "%d:%s, " % (site["id"], site["name"])
+    print all_sites
+    select = raw_input('input a number in the sites to select, or enter 0 to create a new one: ')
+    try:
+        num = int(select)
+        if num <= len(sites) and num > 0:
+            sitedata = sites[num - 1]
+            site = Site(**sitedata)
+            return site
+        elif num == 0:
+            newsite()
+        else:
+            print 'number is not in the sites list.'
+            exit(0)
+    except:
+        print 'enter a number in the sites list.'
         exit(0)
 
 def out_put_in_chapters(urls, site):
@@ -33,10 +120,11 @@ def out_put_in_chapters(urls, site):
         soup = BeautifulSoup(html)
         soup1 = site.get_soup(soup)
         filename = '%s/chapter_%d.txt' % (book_name, (urls.index(i) + 1))
-        f = file(filename, 'w')
-        text = strip_tags(str(soup1))
-        f.write(text)
-        f.close
+        f = file(filename, 'a')
+        for i in soup1:
+            text = strip_tags(str(i))
+            f.write(text)
+        f.close()
 
 def out_put_in_one(urls, site):
     filename = '%s/%s' % (book_name, book_name)
@@ -45,11 +133,13 @@ def out_put_in_one(urls, site):
         html = urllib2.urlopen(i).read()
         soup = BeautifulSoup(html)
         soup1 = site.get_soup(soup)
-        text = strip_tags(str(soup1))
-        f.write(text)
+        for i in soup1:
+            text = strip_tags(str(i))
+            f.write(text)
     f.close()
 
 def get_text():
+    site = get_site()
     global book_name
     book_name = raw_input('Enter name of the book: ')
     try:
@@ -60,7 +150,6 @@ def get_text():
     if how_to_save != '1' and how_to_save != '2':
         print 'Enter 1 or 2.'
         exit(0)
-    site = get_site()
     try:
         urls = site.get_urls()
     except ValueError:
--- sites.py
+++ sites.py
@@ -2,24 +2,94 @@ from bs4 import BeautifulSoup
 import urllib2
 
 class Site():
-    def get_urls(self):
-        pass
+    def __init__(
+                 self,
+                 id,
+                 name,
+                 root_ele,
+                 root_attr,
+                 root_value,
+                 link_ele,
+                 link_attr,
+                 link_value,
+                 ele,
+                 attr,
+                 value,
+                 son_ele,
+                 son_attr,
+                 son_value,
+                 ):
+        self.id = id
+        self.name = name
+        self.root_ele = root_ele
+        self.root_attr = root_attr
+        self.root_value = root_value
+        self.link_ele = link_ele
+        self.link_attr = link_attr
+        self.link_value = link_value
+        self.ele = ele
+        self.attr = attr
+        self.value = value
+        self.son_ele = son_ele
+        self.son_attr = son_attr
+        self.son_value = son_value
 
-    def get_soup(self, soup):
-        pass
-
-class TianYa(Site):
-    def get_urls(self):
+    def url_selector(self):
         enter_url = raw_input('Enter Url Of The Book: ')
-        html = urllib2.urlopen(enter_url).read()
+#         self.root_ele = None
+#         self.root_attr = None
+#         self.root_value = None
+#         self.link_ele = None
+#         self.link_attr = None
+#         self.link_value = None
+        return enter_url
+
+#     def chapter_selector(self):
+#         self.ele = None
+#         self.attr = None
+#         self.value = None
+
+    def get_urls(self):
+        root_url = self.url_selector()
+        html = urllib2.urlopen(root_url).read()
         urls = []
         soup = BeautifulSoup(html)
-        soup1 = soup.find('dl')
-        url_got = soup1.find_all("a", href=True)
+        soup1 = soup.find_all(self.root_ele, attrs={self.root_attr:self.root_value})
+        url_got = []
+        for i in soup1:
+            url_got.append(i.find_all(self.link_ele, attrs={self.link_attr:self.link_value}, href=True))
         for i in url_got:
-            urls.append(enter_url + str(i['href']))
+            for a in i:
+                urls.append(root_url + str(a['href']))
         return urls
 
     def get_soup(self, soup):
-        soup1 = soup.find("div", id="main")
+#         self.chapter_selector()
+        soup_list = soup.find_all(self.ele, attrs={self.attr:self.value})
+        if self.son_ele:
+            i = []
+            for item in soup_list:
+                p = item.find_all(self.son_ele, attrs={self.son_attr:self.son_value})
+                for a in p:
+                    i.append(a)
+            soup1 = i
+        else:
+            soup1 = soup_list
         return soup1
+
+
+# class TianYa(Site):
+#     def url_selector(self):
+#         enter_url = raw_input('Enter Url Of The Book: ')
+#         self.root_ele = "dl"
+#         self.link_ele = "a"
+#         return enter_url
+#
+#     def chapter_selector(self):
+#         self.ele = "div"
+#         self.attr = "id"
+#         self.value = "main"
+#         self.son_ele = "p"
+
+
+
